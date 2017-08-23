1d37d467354ec216860d661bafdd68fde748b24a
Primalfido <primalfido@tubgirl.com>
2017-08-23 18:34:36 +0100

    Initial Commit

---
+++ starfox_decoder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  starfox_decoder
+//
+//  Created by Ahmed Al-Refaie on 23/08/2017.
+//  Copyright © 2017 Primalfido. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
